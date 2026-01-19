d42179df8fc92a2b291118966cf1b743b5496593
Indra saputra idrus <indrasaputraidrus@gmail.com>
2026-01-19 19:42:27 +0800

    create first island

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "preact/hooks"
+
+export default function Greeting({ messages }: { messages: Array<string> }) {
+  const randomMessage = () => messages[Math.floor(Math.random() * messages.length)]
+  const [greeting, setGreeting] = useState(messages[0])
+
+  useEffect(() => {
+    console.log(greeting)
+  }, [greeting])
+
+  return (
+    <div>
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>
+        New Greeting
+      </button>
+    </div>
+  )
+}
